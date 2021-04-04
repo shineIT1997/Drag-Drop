44fd78f45b1174eb0c0c80c9ac6d41dcf109927a
Đinh Thành Đạt <thanhdatit1997@gmail.com>
2021-04-05 01:09:29 +0700

    webpack develop config

--- webpack/development.config.js
+++ webpack/development.config.js
@@ -8,12 +8,14 @@
 process.env.NODE_ENV = 'development'
 
 const path = require('path')
+const paths = require('../config/paths')
 const openBrowser = require('react-dev-utils/openBrowser')
 const commonConfig = require('./common.config')
 const getHttpsConfig = require('../config/getHttpsConfig');
 
-
 const APP_PORT = process.env.REACT_APP_PORT || 4000
+
+const host = process.env.HOST || '0.0.0.0';
 
 
 module.exports = {
@@ -23,20 +25,55 @@ module.exports = {
   mode: 'development',
 
   // Recommended by https://webpack.js.org/configuration/devtool/
-  devtool: 'eval-source-map',
+  devtool: 'cheap-module-source-map',
 
   devServer: {
-    publicPath: '/',
-    contentBase: path.resolve(__dirname ,'../public' ),
-    host: '0.0.0.0',
+    publicPath: paths.publicPath,
+    contentBase: paths.appPublic,
+    contentBasePublicPath: paths.publicUrlOrPath,
+    // By default files from `contentBase` will not trigger a page reload.
+    watchContentBase: true,
+    // contentBase: path.resolve(__dirname ,'../public' ),
+    host,
+    hot: true,
+    quiet: true,
+    inline: true,
+    compress: true,
     port: APP_PORT,
+    https: getHttpsConfig(),
     historyApiFallback: true,
     watchOptions: {
       ignored: /node_modules/
     },
+    proxy: {
+      '/api': {
+        target: process.env.API_PROXY || 'https://api.onib.link',
+        secure: false,
+        changeOrigin: true
+      }
+    },
+
     after: () => {
       openBrowser && openBrowser(`http://127.0.0.1:${APP_PORT}`)
     }
+  },
+
+  output: {
+    // The build folder.
+    path:  undefined,
+    // Add /* filename */ comments to generated require()s in the output.
+    pathinfo: true,
+    // There will be one main bundle, and one file per asynchronous chunk.
+    // In development, it does not produce real files.
+    filename: 'static/js/bundle.js',
+
+    // There are also additional JS chunk files if you use code splitting.
+    chunkFilename: 'static/js/[name].chunk.js',
+    // webpack uses `publicPath` to determine where the app is being served from.
+    // It requires a trailing slash, or the file assets will get an incorrect path.
+    // We inferred the "public path" (such as / or /my-project) from homepage.
+    publicPath: paths.publicUrlOrPath,
+    // Point sourcemap entries to original disk location (format as URL on Windows)
   },
 
   // These are the "entry points" to our application.
